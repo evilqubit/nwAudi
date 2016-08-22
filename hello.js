25a25c4922e7fd20078a8566818e6f8515ddbec0
Carlo Khanati <carlo.khanati@gmail.com>
2016-08-22 17:07:43 +0300

    Initial examples

---
+++ hello.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function(request,response) {
+    var newFile = fs.createWriteStream("readme_copy.md");
+    var fileBytes = request.headers['content-length'];
+    var uploadedByte = 0;
+    request.on('readable',function() {
+        var chunk = null;
+        while (null !== (chunk = request.read())){
+            uploadedByte += chunk.length;
+            var progress = (uploadedByte / fileBytes) * 100;
+            response.write("progress:" + parseInt(progress,10) + "%\n");
+        }
+    });
+
+    request.pipe(newFile);
+   
+    request.on('end',function(){
+        response.end('uploaded4');
+    });    
+    }).listen(8080,function() {
+    console.log('listining on port 8080')});
